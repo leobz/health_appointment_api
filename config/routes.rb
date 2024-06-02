d3e4e1d8012fd2158519e019927461eb601d8d90
Leo Bazan <leo.bazan.x@gmail.com>
2024-06-02 20:10:16 -0300

    feat (draft): (WorkingHours) add route + empty controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api do
+    namespace :v1 do
+
+      resources :doctors, only: [:show] do
+        resources :working_hours, only: [:index], module: :doctors
+      end
+    end
+  end
 end

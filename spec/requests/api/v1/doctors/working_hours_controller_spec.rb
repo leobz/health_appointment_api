b3cd1b3fee4862d3ba49f8c633625441360d5a47
Leo Bazan <leo.bazan.x@gmail.com>
2024-06-03 00:19:12 -0300

    test + feat: WorkingHours Controller

--- spec/requests/api/v1/doctors/working_hours_controller_spec.rb
+++ spec/requests/api/v1/doctors/working_hours_controller_spec.rb
@@ -1,17 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'WorkingHoursController',  type: :request do
+  let!(:doctor)  {
+    Doctor.create(
+      full_name: 'John Doe',
+      working_hours: [
+        new_working_hour('monday', 9,17),
+        new_working_hour('friday', 9,17)
+      ]
+    )
+  }
+
   it 'Index' do
-    get api_v1_doctor_working_hours_path(doctor_id: 1)
+    get api_v1_doctor_working_hours_path(doctor_id: doctor.id)
+
     expect(response).to have_http_status(200)
     expect(response.parsed_body).to match(
-      {
-        'doctor_id' => '1',
-        'working_hours' => [
-          {'day' => 'monday', 'start_time' => '09:00', 'end_time' => '17:00'},
-          {'day' => 'friday', 'start_time' => '09:00', 'end_time' => '17:00'}
-        ]
-      }
+      [
+        {'day' => 'monday', 'start_time' => '09:00', 'end_time' => '17:00'},
+        {'day' => 'friday', 'start_time' => '09:00', 'end_time' => '17:00'}
+      ]
     )
   end
+end
+
+# ***************+  Helper Methods  ******************#
+
+def new_working_hour(day, start_hour, end_hour)
+  WorkingHour.new(
+    day: day,
+    start_time: Time.zone.local(2000, 1, 1, start_hour, 0, 0),
+    end_time: Time.zone.local(2000, 1, 1, end_hour, 0, 0)
+  )
 end

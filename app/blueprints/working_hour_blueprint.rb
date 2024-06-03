225cc7f3045340312557692d5d5203d9a422eaaa
Leo Bazan <leo.bazan.x@gmail.com>
2024-06-03 00:19:12 -0300

    test + feat: WorkingHours Controller

--- app/blueprints/working_hour_blueprint.rb
+++ app/blueprints/working_hour_blueprint.rb
@@ -1,5 +1,11 @@
 class WorkingHourBlueprint < Blueprinter::Base
-  identifier :id
+  identifier :day
 
-  fields :day, :start_time, :end_time
+  field :start_time do |working_hour, _opts|
+    working_hour.start_time.strftime("%H:%M")
+  end
+
+  field :end_time do |working_hour, _opts|
+    working_hour.end_time.strftime("%H:%M")
+  end
 end

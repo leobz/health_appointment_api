400d3c6cdcfba4137e4ff977759411e7e81b2375
Leo Bazan <leo.bazan.x@gmail.com>
2024-06-04 09:21:19 -0300

    fix: (WorkingHour) test + model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,5 +2,5 @@ class Doctor < ApplicationRecord
   has_many :working_hours
   has_many :appointments
 
-  validates :full_name, presence: true
+  validates :first_name, :last_name, presence: true
 end

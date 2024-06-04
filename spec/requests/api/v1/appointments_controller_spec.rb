f3d0c43da45032cc611f909fae90bb674bd54cd7
Leo Bazan <leo.bazan.x@gmail.com>
2024-06-04 16:02:05 -0300

    test + feat: (Appointment) controller

--- spec/requests/api/v1/appointments_controller_spec.rb
+++ spec/requests/api/v1/appointments_controller_spec.rb
@@ -1,38 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'Appointment',  type: :request do
-  let!(:doctor)  {
-    Doctor.create(
-      first_name: 'John',
-      last_name: 'Doe',
-      working_hours: [
-        new_working_hour('monday', 9,17),
-        new_working_hour('friday', 9,17)
-      ]
-    )
-  }
-
-  let!(:patient) { Patient.create(first_name: 'John', last_name: 'Smith') }
+  let!(:doctor)  { new_doctor  [] }
+  let!(:patient) { new_patient }
+  let!(:valid_attributes) { {
+    doctor_id: doctor.id,
+    patient_id: patient.id,
+    start_time: date_time_hour(2024,1,1,15)
+  } }
 
   describe 'Create' do
     it 'creates a new appointment' do
-      body = {
-        patient_id: patient.id,
-        doctor_id: doctor.id,
-        start_time: Time.zone.local(2024, 1, 1, 15, 0, 0)
-      }
-
       expect {
         post(
           api_v1_appointments_path,
           headers: { 'Content-Type': 'application/json' },
-          params: body.to_json
+          params: valid_attributes.to_json
         )
       }.to change(Appointment, :count).by(1)
 
       #***************************** DB Validation *********************************
       appointment = Appointment.last
-      expect(appointment.start_time).to eq(body[:start_time])
+      expect(appointment.start_time).to eq(valid_attributes[:start_time])
       expect(appointment.doctor_id).to  eq(doctor.id)
       expect(appointment.patient_id).to eq(patient.id)
 
@@ -47,6 +36,42 @@ RSpec.describe 'Appointment',  type: :request do
       )
     end
   end
+
+  describe 'Update' do
+    let!(:doctor_2) { new_doctor [] }
+    let!(:patient_2) { new_patient }
+
+    it 'updates an appointment' do
+      appointment = Appointment.create valid_attributes
+
+      body = {
+        doctor_id: doctor_2.id,
+        patient_id: patient_2.id,
+        start_time: date_time_hour(2024,1,2,12)
+      }
+
+      put(
+        api_v1_appointment_path(appointment),
+        headers: { 'Content-Type': 'application/json' },
+        params: body.to_json
+      )
+
+      #***************************** DB Validation *********************************
+      appointment.reload
+      expect(appointment.start_time).to eq(body[:start_time])
+      expect(appointment.doctor_id).to  eq(doctor_2.id)
+      expect(appointment.patient_id).to eq(patient_2.id)
+
+      #**************************** Controller Validation **************************
+      expect(response).to have_http_status(:ok)
+      expect(response.parsed_body).to match({
+        'id' => appointment.id,
+        'start_time' => "2024-01-02T12:00:00Z",
+        'doctor_id' => doctor_2.id,
+        'patient_id' => patient_2.id}
+      )
+    end
+  end
 end
 
 # ***************+  Helper Methods  ******************#
@@ -57,4 +82,16 @@ def new_working_hour(day, start_hour, end_hour)
     start_time: Time.zone.local(2000, 1, 1, start_hour, 0, 0),
     end_time: Time.zone.local(2000, 1, 1, end_hour, 0, 0)
   )
+end
+
+def new_doctor(working_hours)
+  Doctor.create first_name: 'John', last_name: 'Smith', working_hours: working_hours
+end
+
+def new_patient
+  Patient.create(first_name: 'John', last_name: 'Smith')
+end
+
+def date_time_hour(year, month, day, hour)
+  Time.zone.local(year,month,day,hour)
 end

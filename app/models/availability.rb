aac8d6f65ddd6ef0b1111950de2f527e7fe9cc96
Leo Bazan <leo.bazan.x@gmail.com>
2024-06-10 17:42:34 -0300

    refactor: extract Availability model logic from AvailabilityService to Availability

---
+++ app/models/availability.rb
@@ -0,0 +1,35 @@
+class Availability
+  attr_accessor :date
+
+  def initialize(slot_time_in_min, date, appointments, working_hours)
+    @date = date
+    @appointments = appointments
+    @working_hours = working_hours
+    @slot_time_in_min = slot_time_in_min
+  end
+
+  def time_slots
+    @time_slots ||= build_time_slots
+  end
+
+  private
+  def build_time_slots
+    time_slots = []
+
+    @working_hours.each do |working_hour|
+      iteration_time = working_hour.start_time
+
+      while iteration_time < working_hour.end_time
+        time_slot = TimeSlot.new(TimeUtils.build_time(date, iteration_time), @slot_time_in_min)
+
+        unless time_slot.overlaps_with_appointment_list?(@appointments)
+          time_slots << time_slot
+        end
+
+        iteration_time += (60 * @slot_time_in_min)
+      end
+    end
+
+    time_slots
+  end
+end
